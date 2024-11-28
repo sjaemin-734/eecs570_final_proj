621d919efb41f8d9776e7fadc22a7a057ff9aa24
Matthew <ngmatt882@gmail.com>
2024-11-28 18:44:05 -0500

    Drafted imply stack

--- verilog/imply_stack.sv
+++ verilog/imply_stack.sv
@@ -1,24 +1,49 @@
 // 5 variables per clause, 1023 clauses
 
 // Imply Stack
-module trace_table # (
-    paramter NUM_VARIABLE = 128,
-    paramter VARIABLE_INDEXES = 8
+module imply_stack # (
+    parameter NUM_VARIABLE = 128,
+    parameter VARIABLE_INDEXES = 8
 )
 (
     input                 clk,
     input                 reset,
+    input                 en,
     input                 push,
     input                 pop,
-    input                 t_type,       // D=0/F=1
     input                 val,          // Assigned to be T or F
     input           [8:0] variable,
-    output logic          last,         // whether its the last to come out during backpropagation
-    output logic          type_out,
-    output logic          val_out,
-    output logic    [8:0] variable_out,
-    output logic          empty,
-    output logic          done
+    output reg          type_out,
+    output reg          val_out,
+    output reg    [8:0] variable_out,
+    output reg            empty
 );
+    localparam bit [NUM_VARIABLE:0] COUNTER_RESET_VAL = 0;
+    integer i;
+
+    // data structure
+    reg [NUM_VARIABLE:0] counter;                               // current index in stack
+    reg [NUM_VARIABLE:0] stack[0:(VARIABLE_INDEXES+2)];         // [val [1], variable index [8:0]]
+
+    always @(posedge clk) begin
+        if (en == 0);
+        else begin
+            if (reset) begin
+                counter <= COUNTER_RESET_VAL;
+                empty <= 1'b1;
+                for (i = 0; i < NUM_VARIABLE; i=i+1) begin
+                    stack[i] = 0;
+                end
+            end else if (reset == 0) begin
+                empty <= counter == 0 ? 1'b1: 1'b0;
+                if () begin
+                end
+                
+            end
+            if ()  
+                
+            end
+        end
+    end
 
 endmodule
