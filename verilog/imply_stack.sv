afdd29d8985013fd7770a3bec459c5736a451cee
Matthew <ngmatt882@gmail.com>
2024-11-28 19:17:57 -0500

    Completed imply stack

--- verilog/imply_stack.sv
+++ verilog/imply_stack.sv
@@ -9,13 +9,12 @@ module imply_stack # (
     input                 clk,
     input                 reset,
     input                 en,
-    input                 push,
-    input                 pop,
+    input                 rw,           // read/pop = 0, write/push = 1
     input                 val,          // Assigned to be T or F
     input           [8:0] variable,
-    output reg          type_out,
-    output reg          val_out,
-    output reg    [8:0] variable_out,
+    output reg            type_out,     //Always 1
+    output reg            val_out,      
+    output reg      [8:0] variable_out,
     output reg            empty
 );
     localparam bit [NUM_VARIABLE:0] COUNTER_RESET_VAL = 0;
@@ -23,7 +22,7 @@ module imply_stack # (
 
     // data structure
     reg [NUM_VARIABLE:0] counter;                               // current index in stack
-    reg [NUM_VARIABLE:0] stack[0:(VARIABLE_INDEXES+2)];         // [val [1], variable index [8:0]]
+    reg [NUM_VARIABLE:0] stack[0:(VARIABLE_INDEXES+1)];         // [val [1], variable index [8:0]]
 
     always @(posedge clk) begin
         if (en == 0);
@@ -31,12 +30,20 @@ module imply_stack # (
             if (reset) begin
                 counter <= COUNTER_RESET_VAL;
                 empty <= 1'b1;
+                val_out <= 1'b1;
                 for (i = 0; i < NUM_VARIABLE; i=i+1) begin
                     stack[i] = 0;
                 end
             end else if (reset == 0) begin
                 empty <= counter == 0 ? 1'b1: 1'b0;
-                if () begin
+                if (rw == 0 & ~empty) begin
+                    variable_out <= stack[counter - 1][NUM_VARIABLE:0]; 
+                    val_out <= stack[counter - 1][NUM_VARIABLE+1];
+                    type_out <= 1'b1;
+                    counter <= counter - 1;
+                end else if (rw == 1) begin
+                    stack[counter] <= [val, variable];
+                    counter <= counter + 1;
                 end
                 
             end
