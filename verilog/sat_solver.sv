85d02793db28dc2e92f7fcacb95577f09f682b42
Matthew <ngmatt882@gmail.com>
2024-11-29 15:43:51 -0500

    Clean up of parameters in clause eval

--- verilog/sat_solver.sv
+++ verilog/sat_solver.sv
@@ -2,23 +2,25 @@
 
 // Partial SAT Evaluator
 module partial_sat_evaluator # (
-    parameter VAR_PER_CLAUSE = 5
+    parameter VAR_PER_CLAUSE = 5,
+    parameter VAR_PER_CLAUSE_INDEX = VAR_PER_CLAUSE - 1
     )(
-    input           [(VAR_PER_CLAUSE - 1):0] unassign,
-    input           [(VAR_PER_CLAUSE - 1):0] clause_mask,
-    input           [(VAR_PER_CLAUSE - 1):0] assignment,
-    input           [(VAR_PER_CLAUSE - 1):0] clause_pole,
+    input           [VAR_PER_CLAUSE_INDEX:0] unassign,
+    input           [VAR_PER_CLAUSE_INDEX:0] clause_mask,
+    input           [VAR_PER_CLAUSE_INDEX:0] assignment,
+    input           [VAR_PER_CLAUSE_INDEX:0] clause_pole,
     output logic    partial_sat    
 );
     // Intermediate values that feed into the OR
     // gate in the partial_sat module
-    logic [(VAR_PER_CLAUSE - 1):0] or_inputs;
+    logic [VAR_PER_CLAUSE_INDEX:0] or_inputs;
+    integer i;
 
     // Since we're dealing with indiviual bits here, bitwise (&) and logical (&&)
     // operators should be interchangable. For consistency, stick to bitwise.
 
     // TODO: check if these should be nonblocking <= or assign statments
-    for(int i = 0; i < VAR_PER_CLAUSE; i = i + 1) begin
+    for(i = 0; i < VAR_PER_CLAUSE; i = i + 1) begin
         assign or_inputs[i] = (~unassign[i] & clause_mask[i]) & ~(assignment[i] & clause_pole[i])
     end
 
@@ -30,34 +32,37 @@ endmodule
 // Unit Clause valuator
 module unit_clause_evaluator # (
     parameter VAR_PER_CLAUSE = 5,
+    parameter VAR_PER_CLAUSE_INDEX = VAR_PER_CLAUSE - 1,
     parameter NUM_VARIABLE = 128,
-    parameter VARIABLE_INDEX = $clog(NUM_VARIABLE)
+    parameter VARIABLE_INDEX = $clog(NUM_VARIABLE) - 1
 
     )(
-    input           [(VAR_PER_CLAUSE - 1):0] unassign,
-    input           [(VAR_PER_CLAUSE - 1):0] clause_mask,
-    input           [(VAR_PER_CLAUSE - 1):0][(VARIABLE_INDEX-1):0] variable,
-    input           [(VAR_PER_CLAUSE - 1):0] clause_pole,
+    input           [VAR_PER_CLAUSE_INDEX:0] unassign,
+    input           [VAR_PER_CLAUSE_INDEX:0] clause_mask,
+    input           [VAR_PER_CLAUSE_INDEX:0][VARIABLE_INDEX:0] variable,
+    input           [VAR_PER_CLAUSE_INDEX:0] clause_pole,
     output logic    new_assignment,
-    output logic    [(VARIABLE_INDEX-1):0] implied_variable,
+    output logic    [VAR_PER_CLAUSE_INDEX:0] implied_variable,
     output logic    is_unit_clause
 );
     // Intermediate values that feed into the encoder
-    logic [(VAR_PER_CLAUSE-1):0] encoder_inputs;
+    logic [VAR_PER_CLAUSE_INDEX:0] encoder_inputs;
     logic [2:0] mux_input;
+
+    integer i;
 
     // Since we're dealing with indiviual bits here, bitwise (&) and logical (&&)
     // operators should be interchangable. For consistency, stick to bitwise.
 
     // TODO: check if these should be nonblocking <= or assign statments
-    for(int i = 0; i < VAR_PER_CLAUSE; i = i + 1) begin
+    for(i = 0; i < VAR_PER_CLAUSE; i = i + 1) begin
         assign encoder_inputs[i] = unassign[i] & clause_mask[i]
     end
 
 
     // Check if only ONE variable has been unassigned
     always_comb begin
-        case (encoder_inputs[4:0]) begin
+        case (encoder_inputs) begin
             5'b00001 : mux_input = 3'b000
             5'b00010 : mux_input = 3'b001
             5'b00100 : mux_input = 3'b010
--- verilog/sub_clause_eval.sv
+++ verilog/sub_clause_eval.sv
@@ -3,24 +3,27 @@
 // Sub Clause Evaluator
 module sub_clause_evaluator # (
     parameter NUM_VARIABLE = 128,
-    parameter VARIABLE_INDEX = $clog(NUM_VARIABLE),
+    parameter VARIABLE_INDEX = $clog(NUM_VARIABLE) - 1,
     parameter VAR_PER_CLAUSE = 5,
     parameter VAR_PER_CLAUSE_INDEX = VAR_PER_CLAUSE - 1
     )(
     input           [VAR_PER_CLAUSE_INDEX:0] unassign,
     input           [VAR_PER_CLAUSE_INDEX:0] clause_mask,
-    input           [VAR_PER_CLAUSE_INDEX:0][(VARIABLE_INDEX-1):0] variable,
+    input           [VAR_PER_CLAUSE_INDEX:0][VARIABLE_INDEX:0] variable,
     input           [VAR_PER_CLAUSE_INDEX:0] clause_pole,
     input           [VAR_PER_CLAUSE_INDEX:0] assignment,
     output logic    new_assignment,
-    output logic    [(VARIABLE_INDEX-1):0] implied_variable,
+    output logic    [VARIABLE_INDEX:0] implied_variable,
     output logic    unit_clause
 );
     // Intermediate values that feed into the AND
     logic partial_sat;
     logic candidate_unit;
 
-    partial_sat_evaluator pse(
+    partial_sat_evaluator # (
+        .VAR_PER_CLAUSE(VAR_PER_CLAUSE)
+    )
+    pse(
         .unassign(unassign),
         .clause_mask(clause_mask),
         .assignment(assignment),
@@ -28,7 +31,10 @@ module sub_clause_evaluator # (
         .partial_sat(partial_sat)    
     );
 
-    unit_clause_evaluator uce(
+    unit_clause_evaluator # (
+        .VAR_PER_CLAUSE(VAR_PER_CLAUSE),
+        .NUM_VARIABLE(NUM_VARIABLE)
+    ) uce(
         .unassign(unassign),
         .clause_mask(clause_mask),
         .variable(variable),
