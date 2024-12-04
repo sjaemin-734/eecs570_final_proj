7a59698c2cfb211566af0dd2f5b9747179b7b400
Matthew Ng <ngmatt@caen-vnc-mi10.engin.umich.edu>
2024-12-04 17:07:33 -0500

    Added eval prep into control test

--- test/control_test.sv
+++ test/control_test.sv
@@ -55,7 +55,6 @@ module control_test;
     logic val_out_vs;
     logic unassign_out_vs;
     // not included in control
-    logic multi_read_vs;
     logic [`VAR_PER_CLAUSE - 1:0][`MAX_VARS_BITS - 1:0] multi_var_in_vs;
     logic [`VAR_PER_CLAUSE - 1:0] multi_val_out_vs; // For Eval Prep
     logic [`VAR_PER_CLAUSE - 1:0] multi_unassign_out_vs; // For Eval Prep
@@ -64,6 +63,16 @@ module control_test;
     logic [`CLAUSE_TABLE_BITS-1:0] end_clause;
     logic read_var_start_end;
     logic [`MAX_VARS_BITS-1:0] var_in_vse;
+
+    // EVAL PREP
+    logic [`CLAUSE_DATA_BITS-1:0] clause_info_in;
+
+    // CLAUSE EVAL
+    logic [`VAR_PER_CLAUSE-1:0] clause_mask_in_ce;
+    logic [`VAR_PER_CLAUSE-1:0] clause_pole_in_ce;
+    logic [`VAR_PER_CLAUSE-1:0][`MAX_VARS_BITS-1:0] variable_in_ce; // Addresses
+    logic [`VAR_PER_CLAUSE-1:0] unassign_in_ce;
+    logic [`VAR_PER_CLAUSE-1:0] val_in_ce;
 
     // DECIDER MEMORY MODULE
     logic [`MAX_VARS_BITS-1:0] var_idx_d;
@@ -195,7 +204,7 @@ module control_test;
         .clock(clock),
         .reset(reset),
         .read(read_vs),
-        .multi_read(multi_read_vs),
+        .multi_read(ce_en),
         .write(write_vs),
         .val_in(val_in_vs),
         .unassign_in(unassign_in_vs),
@@ -206,6 +215,26 @@ module control_test;
         .unassign_out(unassign_out_vs),
         .multi_val_out(multi_val_out_vs),
         .multi_unassign_out(multi_unassign_out_vs)
+    );
+
+    eval_prep ep (
+        .clause_info_in(clause_info_in),
+        .unassign_in(multi_unassign_out_vs), // From Var State Table
+        .val_in(multi_val_out_vs), // From Var State Table
+
+        .clock(clock),
+        .reset(reset_bcp),
+        .en(bcp_en),
+
+        .idx_out(multi_var_in_vs), // To Var State Table
+
+        // To Clause Evaluator
+        .clause_mask_out(clause_mask_in_ce),
+        .clause_pole_out(clause_pole_in_ce),
+        .variable_out(variable_in_ce), // Addresses
+        .unassign_out(unassign_in_ce),
+        .val_out(val_in_ce),
+        .es_en(ce_en)
     );
 
     // Clock generation
@@ -305,6 +334,7 @@ module control_test;
     always @(posedge clock) begin
         $display("INITIALIZE: reset = %0b start = %0b state = %0d \
                 \nBCP_CORE: bcp_busy = %0b conflict = %0b bcp_clause_idx = %0d bcp_clause_id = %0d reset_bcp = %0d bcp_en = %0b \
+                \nCLAUSE_BCP_INFO: id = %0d mask = %0b pole = %0b var1 = %0d var2 = %0d var3 = %0d var4 = %0d var5 = %0d \
                 \nIMPLY: empty = %0b var_out = %0d val_out = %0b type_out = %0b pop = %0b \
                 \nTRACE: empty = %0b var_out = %0d val_out = %0b type_out = %0b pop = %0b push = %0b var_in = %0d val_in = %0b type_in = %0b \
                 \nVAR STATE: write = %0b var_in = %0d val_in = %0b unassign_in = %0b \
@@ -312,6 +342,7 @@ module control_test;
                 \nRESULTS: sat = %0b unsat %0b\n",
                 reset, start, state_out,
                 bcp_busy, conflict, bcp_clause_idx, bcp_clause_id, reset_bcp, bcp_en,
+                bcp_clause_id, clause_info_in[`CLAUSE_DATA_BITS-1:`CLAUSE_DATA_BITS-5], clause_info_in[`CLAUSE_DATA_BITS-6:`CLAUSE_DATA_BITS-10], clause_info_in[`MAX_VARS_BITS*5-1:`MAX_VARS_BITS*4], clause_info_in[`MAX_VARS_BITS*4-1:`MAX_VARS_BITS*3], clause_info_in[`MAX_VARS_BITS*3-1:`MAX_VARS_BITS*2], clause_info_in[`MAX_VARS_BITS*2-1:`MAX_VARS_BITS], clause_info_in[`MAX_VARS_BITS-1:0], 
                 empty_imply, var_out_imply, val_out_imply, type_out_imply, pop_imply,
                 empty_trace, var_out_trace, val_out_trace, type_out_trace, pop_trace, push_trace, var_in_trace,val_in_trace,type_in_trace,
                 write_vs, var_in_vs, val_in_vs, unassign_in_vs,
@@ -331,6 +362,8 @@ module control_test;
         end_clause = var_start_end_table[var_in_vse][`CLAUSE_TABLE_BITS-1:0];
 
         bcp_clause_id = clause_table[bcp_clause_idx];
+
+        clause_info_in = clause_database[bcp_clause_id];
 
         var_idx_d = dec_idx_d_in;
         push_trace = push_trace_c | push_trace_test;
@@ -365,6 +398,8 @@ module control_test;
         INITIALIZE_VAR_START_END(101,0,2);
         INITIALIZE_CLAUSE_TABLE(0,1);
         INITIALIZE_CLAUSE_TABLE(1,2);
+        INITIALIZE_CLAUSE_DATABASE(1, 5'b11000, 5'b00000, 1, 2, 0, 0 ,0);
+        INITIALIZE_CLAUSE_DATABASE(2, 5'b11000, 5'b10000, 1, 2, 0, 0 ,0);
         clock = 0;
         reset = 1;
 
