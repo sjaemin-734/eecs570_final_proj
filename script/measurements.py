14f664d953d3f6992690fd1c159aaedbd39755d8
Jae Min Shin <sjaemin@beyster1620p46.engin.umich.edu>
2024-12-08 20:22:59 -0500

    Modified measurements.py to create histogram

--- script/measurements.py
+++ script/measurements.py
@@ -1,75 +1,54 @@
-import os
-import sys
+import numpy as np
 import random
+import matplotlib.pyplot as plt
 
+# x: num devices, y: cycles
+# histogram of data
 
-data = []
+file = open("old_combined_output.txt", 'r')
+num_samples = 1500
 
-file1 = open("combined_output.txt", 'r')
+def sample_and_plot():
 
-
-print("ONE DEVICE")
-for line in file1:
-    line = line.split(" ")
-
-    if line[0] == "$finish" and line[2] == "simulation":
-        data.append(int(line[-1])//10)
-
-# print(data)
-
-def average(numbers):
-    return sum(numbers)/len(numbers)
-
-print(average(data))
-
-
-print("TWO DEVICES")
-device_pair = []
-
-for i in range(150):
-    pair = []
-    for j in range(2):
-        index = random.randint(1, len(data))
-        pair.append(data[index-1])
-    device_pair.append(min(pair))
-
-# print(device_pair)
-print(average(device_pair))
-
-
-print("FOUR DEVICES")
-device_four = []
-for i in range(150):
-    group = []
-    for j in range(4):
-        index = random.randint(1, len(data))
-        group.append(data[index-1])
-    device_four.append(min(group))
-
-print(average(device_four))
-
-
-print("EIGHT DEVICES")
-device_eight = []
-for i in range(150):
-    group = []
-    for j in range(8):
-        index = random.randint(1, len(data))
-        group.append(data[index-1])
-    device_eight.append(min(group))
-
-print(average(device_eight))
-
-
-print("SIXTEEN DEVICES")
-device_16 = []
-for i in range(150):
-    group = []
-    for j in range(8):
-        index = random.randint(1, len(data))
-        group.append(data[index-1])
-    device_16.append(min(group))
-
+    data = []
+    device_pair = []
+    avgs = []
     
-print(average(device_16))
-        
+    numbers_str = ['TWO', 'FOUR', 'EIGHT', 'SIXTEEN']
+    numbers = [2, 4, 8, 16]
+    
+    # Reading data from file
+    for line in file:
+        line = line.split(" ")
+
+        if line[0] == "$finish" and line[2] == "simulation":
+            data.append(int(line[-1])//10)
+
+    print(data)
+    print(np.average(data))
+
+    for i in range(len(numbers)):
+        for j in range(num_samples):
+            pair = []
+            for k in range(numbers[i]):
+                index = random.randint(1, len(data))
+                pair.append(data[index-1])
+            device_pair.append(np.min(pair))
+        print(f"{numbers_str[i]} DEVICES")
+        avg = np.average(device_pair)
+        avgs.append(avg)
+        print(avg)
+
+    plt.hist(data, 30)
+    plt.xlabel('Cycles')
+    plt.ylabel('Count')
+    plt.title('Distribution of Execution Times on uf50n03.cnf')
+    plt.show()
+    # plt.savefig('histogram.png')
+    
+
+    return avgs
+
+if __name__ == '__main__':
+    y = sample_and_plot()
+    print(y)
